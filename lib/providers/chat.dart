908aea65eac747c8a95c859a31a98e4a3fc859c3
muza <muzallyefah@gmail.com>
2023-10-25 01:51:33 +0800

    fix: change garden leave text

--- lib/providers/chat.dart
+++ lib/providers/chat.dart
@@ -12,7 +12,7 @@ class ChatProvider with ChangeNotifier {
   final List<Topic> _topics = [
     Topic('Sexual Harassment', 'harassment', Assets.harassment),
     Topic('Bullying', 'bully', Assets.bully),
-    Topic('Garden Leave', 'termination', Assets.leave),
+    Topic('NDA', 'nda', Assets.leave),
     Topic('Office Ethics', 'ethics', Assets.safety),
   ];
   Topic? _selectedTopic;
